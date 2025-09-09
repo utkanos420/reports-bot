a928f76abde7ef28324fa57f42b11b660faa2044
Максим <daseoffc@gmail.com>
2025-09-09 14:08:39 +0300

    build: добавлена поддержка Redis через Docker

--- app/loader.py
+++ app/loader.py
@@ -1,10 +1,17 @@
 from aiogram import Bot, Dispatcher
 from aiogram.enums import ParseMode
 from aiogram.client.default import DefaultBotProperties
-from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.fsm.storage.redis import RedisStorage
 from settings.config import settings
+from redis.asyncio import Redis
 
+redis = Redis(
+    host="localhost",
+    port=6379,
+    decode_responses=True
+)
+
+storage = RedisStorage(redis=redis)
 
 bot = Bot(token=settings.bot_api_key, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
-storage = MemoryStorage()
 dp = Dispatcher(storage=storage)
