a85963043f3730c229f0845f7953310e23a2358f
Максим <daseoffc@gmail.com>
2025-09-09 23:56:40 +0300

    refactor: изменены ответы бота, в частности те, что были в логике бота, не вынесены в .html

--- app/bot/handlers/users/user.py
+++ app/bot/handlers/users/user.py
@@ -5,9 +5,9 @@ from aiogram.filters import StateFilter, Command
 from database.methods.queries import DBMethods
 from templates import render_template
 
-from bot.keyboards.keyboard_utils import (floor_1_audiences, floors_keyboard, reason_keyboard, 
-                                          skip_report_description, floor_2_audiences, floor_3_audiences, 
-                                          floor_4_audiences, create_report_keyboard)
+from bot.keyboards.keyboard_utils import (floor_1_cabs, floors_keyboard, reason_keyboard, 
+                                          skip_report_description, floor_2_cabs, floor_3_cabs, 
+                                          floor_4_cabs, create_report_keyboard)
 from bot.states.users import UserStates, Anketa
 from bot.utils.sender import send_report_to_admins
 from bot.utils.filter import anti_spam_handler
@@ -70,18 +70,14 @@ async def handle_floor_button(callback: CallbackQuery, state: FSMContext):
     floor = int(callback.data.removeprefix("floor_"))
 
     floor_keyboards = {
-        1: floor_1_audiences,
-        2: floor_2_audiences,
-        3: floor_3_audiences,
-        4: floor_4_audiences,
+        1: floor_1_cabs,
+        2: floor_2_cabs,
+        3: floor_3_cabs,
+        4: floor_4_cabs,
     }
 
-    if floor not in floor_keyboards:
-        await callback.answer("Неизвестный этаж.")
-        return
-
     await callback.message.answer(
-        "<b>Выберите кабинет:</b>",
+        text=render_template("user/choose_floor.html"),
         reply_markup=floor_keyboards[floor](),
         parse_mode="HTML"
     )
@@ -90,57 +86,57 @@ async def handle_floor_button(callback: CallbackQuery, state: FSMContext):
     await callback.message.delete()
 
 
-@user_main_router.callback_query(F.data.startswith("audience_"), StateFilter(Anketa.get_auditory))
+@user_main_router.callback_query(F.data.startswith("cabinet_"), StateFilter(Anketa.get_auditory))
 async def handle_cabinet_button(callback: CallbackQuery, state: FSMContext):
 
     if callback.message.chat.type != "private" or await db_methods.user_is_muted(callback.from_user.id):
         return
 
-    audience_number = callback.data.removeprefix("audience_")
+    audience_number = callback.data.removeprefix("cabinet_")
 
-    await callback.message.answer("<b>Выберите вашу проблему из списка:</b>", reply_markup=reason_keyboard(), parse_mode="HTML")
+    await callback.message.answer(text=render_template("user/choose_reason.html"), reply_markup=reason_keyboard(), parse_mode="HTML")
     await state.update_data(audi=audience_number)
     await state.set_state(Anketa.get_trouble)
     await callback.message.delete()
 
 
-@user_main_router.callback_query(F.data == 'sound', StateFilter(Anketa.get_trouble))
+@user_main_router.callback_query(F.data == "card", StateFilter(Anketa.get_trouble))
 async def handle_reason_button(callback: CallbackQuery, state: FSMContext):
 
     if callback.message.chat.type != "private" or await db_methods.user_is_muted(callback.from_user.id):
         return
 
     await callback.message.answer(render_template("user/problem_description.html"), parse_mode="HTML", reply_markup=skip_report_description())
-    await state.update_data(trouble="sound")
+    await state.update_data(trouble="Проходка")
     await state.set_state(Anketa.description)
     await callback.message.delete()
 
 
-@user_main_router.callback_query(F.data == 'pc', StateFilter(Anketa.get_trouble))
+@user_main_router.callback_query(F.data == 'devices', StateFilter(Anketa.get_trouble))
 async def handle_reason_button(callback: CallbackQuery, state: FSMContext):
 
     if callback.message.chat.type != "private" or await db_methods.user_is_muted(callback.from_user.id):
         return
 
     await callback.message.answer(render_template("user/problem_description.html"), parse_mode="HTML", reply_markup=skip_report_description())
-    await state.update_data(trouble="pc")
+    await state.update_data(trouble="Оборудование")
     await state.set_state(Anketa.description)
     await callback.message.delete()
 
 
-@user_main_router.callback_query(F.data == 'projector', StateFilter(Anketa.get_trouble))
+@user_main_router.callback_query(F.data == 'devices_soft', StateFilter(Anketa.get_trouble))
 async def handle_reason_button(callback: CallbackQuery, state: FSMContext):
 
     if callback.message.chat.type != "private" or await db_methods.user_is_muted(callback.from_user.id):
         return
 
     await callback.message.answer(render_template("user/problem_description.html"), parse_mode="HTML", reply_markup=skip_report_description())
-    await state.update_data(trouble="проектор")
+    await state.update_data(trouble="Настройка оборудования")
     await state.set_state(Anketa.description)
     await callback.message.delete()
 
 
-@user_main_router.callback_query(F.data == 'другое', StateFilter(Anketa.get_trouble))
+@user_main_router.callback_query(F.data == "other", StateFilter(Anketa.get_trouble))
 async def handle_reason_button(callback: CallbackQuery, state: FSMContext):
 
     if callback.message.chat.type != "private" or await db_methods.user_is_muted(callback.from_user.id):
@@ -159,7 +155,7 @@ async def handle_skip_description_button(callback: CallbackQuery, state: FSMCont
 
     await callback.message.answer(render_template("user/report_created.html"), parse_mode="HTML")
 
-    await state.update_data(desc="Комментарий отсутсвует")
+    await state.update_data(description="Комментарий отсутсвует")
 
     data = await state.get_data()
     user_id = data.get('id')
@@ -169,7 +165,7 @@ async def handle_skip_description_button(callback: CallbackQuery, state: FSMCont
         report_floor=data['floor'],
         report_cabinet=data['audi'],
         report_reason=data['trouble'],
-        report_description=data['desc']
+        report_description=data['description']
     )
 
     await state.set_state(UserStates.main_state)
@@ -201,7 +197,7 @@ async def handle_adding_report_description(message: types.Message, state: FSMCon
         report_floor=data['floor'],
         report_cabinet=data['audi'],
         report_reason=data['trouble'],
-        report_description=data['desc']
+        report_description=data['description']
     )
 
     await state.set_state(UserStates.main_state)
